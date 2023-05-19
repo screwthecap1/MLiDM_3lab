91baa79d244d541cb38aa4b685645f7e313d0c9f
Klim Borodin <123664193+screwthecap1@users.noreply.github.com>
2023-05-19 19:00:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,79 @@
-/**
- * Валидация параметров ввода
- * @param masA
- * @param masB
- * @param masOfMas
- */
-function validateElement(masA, masB, masOfMas) {
-  let isValid = true;
+/* Валидация параметров ввода (Mas_A, Mas_B, Matrix) */
+function validateInput(Mas_A, Mas_B, Matrix) {
+    let isValid = true;
 
-  if (masA.length != new Set(masA).size) {
-    document.getElementById("result").innerHTML +=
-      "Массив X содержит повторения <br>";
-    isValid = false;
-  }
-
-  if (masB.length != new Set(masB).size) {
-    document.getElementById("result").innerHTML +=
-      "Массив Y содержит повторения <br>";
-    isValid = false;
-  }
-
-  if (masOfMas.length != masB.length) {
-    document.getElementById("result").innerHTML +=
-      "Количество строк матрицы смежности не совпадает с массивами <br>";
-    isValid = false;
-  }
-
-  if (masOfMas[0].split(" ").length != masA.length) {
-    document.getElementById("result").innerHTML +=
-      "Количество столбцов матрицы смежности не совпадает с массивами <br>";
-    isValid = false;
-  }
-  let isNum = true;
-  for (let i = 0; i < masOfMas.length; ++i) {
-    let elem = masOfMas[i].split(" ");
-    if (elem.length != masA.length) {
-      document.getElementById("result").innerHTML +=
-        "Неверное количество элементов в " + (i + 1) + " строке <br>";
-      isValid = false;
+    if (Mas_A.length !== new Set(Mas_A).size) {
+        document.getElementById("result").innerHTML += "Массив A содержит повторения<br>";
+        isValid = false;
     }
-    if (isNum) {
-      for (let sym of elem) {
-        if (!(sym == 0 || sym == 1)) {
-          document.getElementById("result").innerHTML +=
-            "Матрица смежности должна содержать только 0 и 1 <br>";
-          isNum = false;
-          isValid = false;
-          break;
+
+    if (Mas_B.length !== new Set(Mas_B).size) {
+        document.getElementById("result").innerHTML += "Массив B содержит повторения<br>";
+        isValid = false;
+    }
+
+    if (Matrix.length !== Mas_A.length) {
+        document.getElementById("result").innerHTML += "Количество строк матрицы смежности не совпадает с размером массива A<br>";
+        isValid = false;
+    }
+
+    const expectedColumnCount = Mas_B.length;
+
+    for (let i = 0; i < Matrix.length; i++) {
+        let row = Matrix[i].split(" ");
+        if (row.length !== expectedColumnCount) {
+            document.getElementById("result").innerHTML += "Неверное количество столбцов в строке " + (i + 1) + "<br>";
+            isValid = false;
         }
-      }
-    }
-  }
 
-  return isValid;
-}
-/**
- * Проверка, являются ли отношения функцией
- * @param masA
- * @param masB
- * @param masOfMas
- * @param type
- */
-function check(masA, masB, masOfMas, type) {
-  let mp = new Map();
-  let isFunction = true;
-  let x = 0;
-  let y = 0;
-  for (let i = 0; i < masOfMas.length; ++i) {
-    let elem = masOfMas[i].split(" ");
-    for (let j = 0; j < elem.length; ++j) {
-      if (type == "AB") (x = i), (y = j);
-      else (x = j), (y = i);
-      if (elem[j] == 1) {
-        if (mp.has(masA[x]) == 0) mp.set(masA[x], new Set());
-        mp.get(masA[x]).add(masB[y]);
-        if (mp.get(masA[x]).size > 1) {
-          isFunction = false;
+        for (let j = 0; j < row.length; j++) {
+            const element = row[j];
+            console.log(element);
+            if (!(element === "0" || element === "1")) {
+                document.getElementById("result").innerHTML += "Матрица смежности должна содержать только 0 и 1<br>";
+                isValid = false;
+                break;
+            }
         }
-      }
     }
-  }
 
-  if (isFunction)
-    for (let elem of mp.keys()) {
-      if (mp.get(elem).size != 1) {
-        isFunction = false;
-        break;
-      }
-    }
-  return isFunction;
+    return isValid;
 }
 
-/**
- * Основная функция
- */
+/* Проверка матрицы смежности на функцию */
+function checkFunction(Mas_A, Mas_B, Matrix) {
+    for (let j = 0; j < Mas_B.length; j++) {
+        let count = 0;
+
+        for (let i = 0; i < Matrix.length; i++) {
+            const row = Matrix[i].split(" ");
+            if (row[j] === "1") {
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/* Основная функция */
 function main() {
-  let masA = document.getElementById("id_masA").value;
-  let masB = document.getElementById("id_masB").value;
-  let masOfMas = document.getElementById("id_masOfMas").value;
-  document.getElementById("result").innerHTML = "Результат: <br>";
-  masA = masA.split(" ");
-  masB = masB.split(" ");
-  masOfMas = masOfMas.split("\n");
-  let IsFunction = true;
+    let Mas_A = document.getElementById("id_MasA").value.trim().split(" ");
+    let Mas_B = document.getElementById("id_MasB").value.trim().split(" ");
+    let Matrix = document.getElementById("id_Matrix").value.split("\n");
 
-  if (!validateElement(masA, masB, masOfMas)) {
-    return;
-  }
+    document.getElementById("result").innerHTML = "Результат:<br>";
 
-  for (let i = 0; i < 2; ++i) {
-    if (i == 1) {
-      IsFunction = check(masA, masB, masOfMas, "BA");
-      document.getElementById("result").innerHTML += "B -> A";
-    } else {
-      IsFunction = check(masB, masA, masOfMas, "AB");
-      document.getElementById("result").innerHTML += "A -> B";
+    if (!validateInput(Mas_A, Mas_B, Matrix)) {
+        return;
     }
-    document.getElementById("result").innerHTML += " отношения ";
-    if (!IsFunction) document.getElementById("result").innerHTML += "не ";
-    document.getElementById("result").innerHTML += "являются функцией <br>";
-  }
-  masA = null;
-  masB = null;
-  masOfMas = null;
-}
 
+    if (checkFunction(Mas_A, Mas_B, Matrix)) {
+        document.getElementById("result").innerHTML += "Матрица смежности является функцией";
+    } else {
+        document.getElementById("result").innerHTML += "Матрица смежности не является функцией";
+    }
+}
